745a62fba1626d0e9234a37137d7cd575d35fb8f
Ben Caunt <bdcaunt@gmail.com>
2022-02-08 20:47:11 -0500

    Start the red duck auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/competetionOpModes/RedDuckAutoRR.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes.competetionOpModes;
+
+import org.firstinspires.ftc.teamcode.templateOpModes.BaseAuto;
+
+public class RedDuckAutoRR extends BaseAuto {
+	@Override
+	public void setStartingPosition() {
+
+	}
+
+	@Override
+	public void setVisionSettings() {
+
+	}
+
+	@Override
+	public void addActions() {
+
+	}
+}
