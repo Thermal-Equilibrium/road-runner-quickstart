dbe7d6628b9519d102e3f011958d9fa90848ade1
Ben Caunt <bdcaunt@gmail.com>
2022-02-14 21:24:43 -0500

    simple servo test program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/testOpModes/ServoTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.opmodes.testOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous
+public class ServoTest extends LinearOpMode {
+
+    Servo bucketServo;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        bucketServo = hardwareMap.get(Servo.class, "bucket");
+        bucketServo.setPosition(0);
+        waitForStart();
+        while (opModeIsActive()) {
+            bucketServo.setPosition(1);
+        }
+    }
+}
